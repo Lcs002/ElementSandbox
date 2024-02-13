5142d79f2c37a72bba728665d857f9384142b3dc
LucasVianna <59148393+Lcs002@users.noreply.github.com>
2024-02-13 01:15:33 +0100

    Fixed Error on Compilation

---
+++ ElementSandbox/InteractionHandler.hh
@@ -0,0 +1,18 @@
+#ifndef INTERACTION_HANDLER_H
+#define INTERACTION_HANDLER_H
+
+#include <map>
+#include <SFML/Graphics/Texture.hpp>
+
+class Interaction;
+class InteractionHandler
+{
+public:
+	void addInteraction(unsigned char id, sf::Texture& texture);
+	Interaction* getInteraction(unsigned char id);
+
+private:
+	std::map<unsigned char, Interaction*> interactions;
+};
+
+#endif
